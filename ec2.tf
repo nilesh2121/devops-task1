31e019c1018c55cdd19a1565be5cbca372ad2e1d
nilesh2121 <nilesh.koli@gmail.com>
2022-07-07 19:24:34 +0530

    new data repo added

--- ec2.tf
+++ ec2.tf
@@ -11,22 +11,22 @@ resource "aws_instance" "webserver" {
     }
 
    # Copy the ssh key to host 
-    # provisioner "remote-exec" {
-    #   inline = [
-    #     "ssh-copy-id /home/devops/.ssh devops@aws_instance.webserver.private_ip"
-    #   ]
+    provisioner "remote-exec" {
+      inline = [
+        "ssh-copy-id /home/devops/.ssh/id_rsa devops@aws_instance.webserver.private_ip"
+      ]
       
     
-    # }
+    }
 
-    # connection {
-    #   type = "ssh"
-    #   host = aws_instance.webserver.private_ip
-    #   user = "devops"
-    #   password = "india@123"
-    #   private_key = file("/home/devops/key/.ssh/id_rsa")
-    #   timeout = "4m"
-    # }
+    connection {
+      type = "ssh"
+      host = aws_instance.webserver.private_ip
+      user = "devops"
+      password = "india@123"
+      private_key = file("/home/devops/key/.ssh/id_rsa")
+      timeout = "4m"
+    }
 
      user_data = file("script/user.sh")
 
@@ -53,22 +53,22 @@ resource "aws_instance" "dbserver" {
     user_data = file("script/user.sh")
 
        # Copy the ssh key to host 
-    # provisioner "remote-exec" {
-    #   inline = [
-    #     "ssh-copy-id /home/devops/.ssh devops@aws_instance.dbserver.private_ip"
-    #   ]
+    provisioner "remote-exec" {
+      inline = [
+        "ssh-copy-id /home/devops/.ssh/id_rsa devops@aws_instance.dbserver.private_ip"
+      ]
       
     
-    # }
+    }
 
-    # connection {
-    #   type = "ssh"
-    #   host = aws_instance.dbserver.private_ip
-    #   user = "devops"
-    #   password = "india@123"
-    #   private_key = file("/home/devops/key/.ssh/id_rsa")
-    #   timeout = "4m"
-    # }
+    connection {
+      type = "ssh"
+      host = aws_instance.dbserver.private_ip
+      user = "devops"
+      password = "india@123"
+      private_key = file("/home/devops/key/.ssh/id_rsa")
+      timeout = "4m"
+    }
 
    
 
