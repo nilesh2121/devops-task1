03e80cc0afecdf3eba57ceebc75c7c8d5410c928
nilesh2121 <nilesh.koli@gmail.com>
2022-07-09 11:00:39 +0530

    new data repo added

--- ec2.tf
+++ ec2.tf
@@ -21,7 +21,7 @@ resource "aws_instance" "webserver" {
     provisioner "remote-exec" {
       inline = [
         "#!/bin/bash",
-        "sshpass -e ssh-copy-id -i ~/.ssh/id_rsa.pub -o StrictHostKeyChecking=no devops@aws_instance.webserver.private_ip"
+        "sshpass -p india@123 'ssh-copy-id '-i ~/.ssh/id_rsa.pub -o StrictHostKeyChecking=no devops@aws_instance.webserver.private_ip"
       ]
 
     
