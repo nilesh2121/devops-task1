129d78b954ac9c5e8d0f418c50f1c2233f519dc0
nilesh2121 <nilesh.koli@gmail.com>
2022-07-09 13:19:54 +0530

    new data repo added key

--- ec2.tf
+++ ec2.tf
@@ -10,20 +10,20 @@ resource "aws_instance" "webserver" {
       Name = "web-server"
     }
 
-    connection {
-      type = "ssh"
-      host = aws_instance.webserver.private_ip
-      user = "ubuntu"
-      # password = "nasa@123"
-      private_key = file("/home/devops/key/.ssh/id_rsa")
-      timeout = "4m"
-    } 
+    # connection {
+    #   type = "ssh"
+    #   host = aws_instance.webserver.private_ip
+    #   user = "ubuntu"
+    #   # password = "nasa@123"
+    #   private_key = file("/home/devops/key/.ssh/id_rsa")
+    #   timeout = "4m"
+    # } 
 
-    provisioner "file" {
-      source = "/home/devops/.ssh/id_rsa"
-      destination = "/home/devops/.ssh/"
+    # provisioner "file" {
+    #   source = "/home/devops/.ssh/id_rsa"
+    #   destination = "/home/devops/.ssh/"
     
-    }
+    # }
 
     
 
