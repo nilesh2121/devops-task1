19d8f9da518f0fd39ab7e193207d37193eb097b7
nilesh2121 <nilesh.koli@gmail.com>
2022-07-12 01:03:54 +0530

    added the host details

--- ec2.tf
+++ ec2.tf
@@ -2,8 +2,7 @@
 resource "aws_instance" "webserver" {
     ami = "ami-08d4ac5b634553e16"
     instance_type = "t2.micro"
-    key_name = "terra-key"
-    # key_name = "mylaptop-us"
+    key_name = "mylaptop-us"
     subnet_id = data.aws_subnet.public-subnet.id
     associate_public_ip_address = true
     vpc_security_group_ids = [aws_security_group.websg.id]
@@ -40,8 +39,7 @@ resource "aws_instance" "webserver" {
 resource "aws_instance" "dbserver" {
     ami = "ami-08d4ac5b634553e16"
     instance_type = "t2.micro"
-    key_name = "terra-key"
-    # key_name = "mylaptop-us"
+    key_name = "mylaptop-us"
     subnet_id = aws_subnet.private_subnet.id
     vpc_security_group_ids = [aws_security_group.dbsg.id]
     tags = {
@@ -78,11 +76,11 @@ resource "aws_instance" "dbserver" {
 
 # added the keypaire location - production
 
-# resource "aws_key_pair" "mylaptop-us" {
-#     key_name = "mylaptop-us"
-#     public_key = file("/home/devops/Key/.ssh/id_rsa.pub")
+resource "aws_key_pair" "mylaptop-us" {
+    key_name = "mylaptop-us"
+    public_key = file("/home/devops/Key/.ssh/id_rsa.pub")
     
-# }
+}
 
 
 # added the keypaire location -- staging 
