c7e6fbfb402bdb5024f59f2ca05d7685c8e4a80d
nilesh2121 <nilesh.koli@gmail.com>
2022-07-13 14:56:57 +0530

    added the host details

--- ec2.tf
+++ ec2.tf
@@ -22,7 +22,7 @@ resource "aws_instance" "webserver" {
 
     provisioner "remote-exec" {
       inline = [
-         #!/bin/bash
+        #!/bin/bash
         "sudo ssh-copy-id devops@aws_instance.webserver.private_ip"
         # "sudo scp devops@10.0.0.147:/home/devops/.ssh/id_rsa.pub devops@aws_instance.dbserver.private_ip"
         
