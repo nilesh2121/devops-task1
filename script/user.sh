0bc1c0a879c18aa5bfc5f858046ae041c1781b56
nilesh2121 <nilesh.koli@gmail.com>
2022-07-13 20:14:47 +0530

    added the host details

--- script/user.sh
+++ script/user.sh
@@ -16,9 +16,7 @@ sudo service sshd restart
 #Add Group to visudoers
 echo "%devops ALL=(ALL) NOPASSWD: ALL" | sudo EDITOR="tee -a" visudo
 
-sudo mkdir /home/devops/.ssh
 
-sudo mv /tmp/id_rsa.pub /home/devops/.ssh/
 
 # # Add Local IAC User
 # sudo useradd -m -p $(openssl passwd -1 <india@123>) devops
